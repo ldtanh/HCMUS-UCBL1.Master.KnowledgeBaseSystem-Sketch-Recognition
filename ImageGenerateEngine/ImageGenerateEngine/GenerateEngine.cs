6c4d1d6483b0fdd8aaa7b0a5baf3229883fc8300
Tyshon-Dang <thson.dang.22@gmail.com>
2021-03-25 10:35:46 +0700

    Tyshon commit ImageGenerateEngine code

---
+++ ImageGenerateEngine/ImageGenerateEngine/GenerateEngine.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace ImageGenerateEngine
+{
+	public class GenerateEngine
+	{
+		public static void GeneratePNG(string imagePath)
+		{
+			for (int i = 0; i < 360; i=i+ Config.Default.RotationAngleIncrement)
+			{
+				Bitmap prototypeImg = new Bitmap(imagePath);
+				prototypeImg.SetResolution(prototypeImg.HorizontalResolution, prototypeImg.VerticalResolution);
+				using (Graphics g = Graphics.FromImage(prototypeImg))
+				{
+					// Set the rotation point to the center in the matrix
+					g.TranslateTransform(prototypeImg.Width / 2, prototypeImg.Height / 2);
+
+					// Rotate
+					g.RotateTransform(i);
+
+					// Restore rotation point in the matrix
+					g.TranslateTransform(-prototypeImg.Width / 2, -prototypeImg.Height / 2);
+
+					// Draw the image on the bitmap
+					g.DrawImage(prototypeImg, new Point(0, 0));
+				}
+
+				//write image
+				if (i < 2)
+				{
+					prototypeImg.Save(imagePath.Replace(Config.Default.Root, Config.Default.GenerateRoot).Replace(".png", "") + "\\" + i + "_degree.png");
+				}
+				else
+				{
+					prototypeImg.Save(imagePath.Replace(Config.Default.Root, Config.Default.GenerateRoot).Replace(".png", "") + "\\" + i + "_degrees.png");
+				}
+			}
+		}
+
+		public static void CreateDirectory(string path)
+		{
+			if (!Directory.Exists(path))
+			{
+				Directory.CreateDirectory(path);
+			}
+		}
+	}
+}
---
+++ ImageGenerateEngine/ImageGenerateEngine/GenerateExecution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageGenerateEngine
+{
+	class GenerateExecution
+	{
+		static void Main()
+		{
+			var watch = System.Diagnostics.Stopwatch.StartNew();
+			try
+			{
+				CheckForStructure();
+				GenerateEngine.CreateDirectory(Config.Default.GenerateRoot);
+				List<string> totalFilesList = new List<string>();
+				string[] subdirectoryEntries = Directory.GetDirectories(Config.Default.Root);
+				foreach (var dir in subdirectoryEntries)
+				{
+					string[] fileArray = Directory.GetFiles(dir, "*.png");
+					totalFilesList.AddRange(fileArray);
+				}
+				totalFilesList.OrderBy(model => model);
+				List<IEnumerable<string>> listOfLists = new List<IEnumerable<string>>();
+				for (int i = 0; i < totalFilesList.Count(); i += 1)
+				{
+					listOfLists.Add(totalFilesList.Skip(i).Take(5));
+				}
+				var a = listOfLists;
+				Parallel.ForEach(listOfLists, obj =>
+				{
+					foreach (var item in obj)
+					{
+						GenerateEngine.CreateDirectory(item.Replace(Config.Default.Root, Config.Default.GenerateRoot).Replace(".png", ""));
+						GenerateEngine.GeneratePNG(item);
+					}
+				});
+				watch.Stop();
+				Console.WriteLine($"Total Execution Time: {ConsoleUtility.ConvertToTime(TimeSpan.FromMilliseconds(watch.ElapsedMilliseconds))}.");
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("An error happend when attemp to generate picture. Please check the cofig file again.");
+				watch.Stop();
+				Console.WriteLine($"Total Execution Time: {ConsoleUtility.ConvertToTime(TimeSpan.FromMilliseconds(watch.ElapsedMilliseconds))}.");
+			}
+		}
+
+		static void CheckForStructure()
+		{
+			try
+			{
+				//Get all subdirectories
+				List<string> totalFilesList = new List<string>();
+				string[] subdirectoryEntries = Directory.GetDirectories(Config.Default.Root);
+			}
+			catch (DirectoryNotFoundException)
+			{
+				Console.WriteLine("Cannot found the directory " + Config.Default.Root + ". Please make that sure you provide correct path.");
+				return;
+			}
+		}
+	}
+}
